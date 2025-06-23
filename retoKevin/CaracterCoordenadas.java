ff645cd115a272c5f0d18d7c05fb9b063ceb8942
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-06-22 21:40:48 -0600

    puede imprimir en vertical

--- retoKevin/CaracterCoordenadas.java
+++ retoKevin/CaracterCoordenadas.java
@@ -39,6 +39,7 @@ public class CaracterCoordenadas {
    * 
    * @return El caracter en cuestion.
    */
+
   public Character getName() {
     return name;
   }
@@ -48,81 +49,50 @@ public class CaracterCoordenadas {
    * 
    * @return Las coodenadas en matriz 2x2.
    */
+
   public Boolean[][] getCoor() {
     return coor;
   }
-  Boolean[][] coorS = new Boolean[9][ancho];
-    coorS[0][0 + 10] = true;
-    coorS[0][1 + 10] = true;
-    coorS[0][2 + 10] = true;
-    coorS[0][3 + 10] = true;
-    coorS[1][0 + 10] = true;
-    coorS[2][0 + 10] = true;
-    coorS[3][0 + 10] = true;
-    coorS[4][0 + 10] = true;
-    coorS[4][1 + 10] = true;
-    coorS[4][2 + 10] = true;
-    coorS[4][3 + 10] = true;
-    coorS[5][3 + 10] = true;
-    coorS[6][3 + 10] = true;
-    coorS[7][3 + 10] = true;
-    coorS[8][0 + 10] = true;
-    coorS[8][1 + 10] = true;
-    coorS[8][2 + 10] = true;
-    coorS[8][3 + 10] = true;
-
-  /**
-   * Mapeo de objetos, cada caracter es un objeto
-   */
-  Boolean[][] coorj = new Boolean[9][ancho];
-  coorj[0][3]=true;
-  coorj[1][3]=true;
-  coorj[2][3]=true;
-  coorj[3][3]=true;coorj[4][3]=true;coorj[5][3]=true;coorj[6][0]=true;coorj[6][3]=true;coorj[7][0]=true;coorj[7][3]=true;coorj[8][0]=true;coorj[8][1]=true;coorj[8][2]=true;coorj[8][3]=true;
-
-
-
-  Boolean[][] coorO = new Boolean[9][ancho];coorO[0][0+5]=true;coorO[0][1+5]=true;coorO[0][2+5]=true;coorO[0][3+5]=true;coorO[1][0+5]=true;coorO[1][3+5]=true;coorO[2][0+5]=true;coorO[2][3+5]=true;coorO[3][0+5]=true;coorO[3][3+5]=true;coorO[2][3+5]=true;coorO[4][0+5]=true;coorO[4][3+5]=true;coorO[5][0+5]=true;coorO[5][3+5]=true;coorO[6][0+5]=true;coorO[6][3+5]=true;coorO[7][0+5]=true;coorO[7][3+5]=true;coorO[8][0+5]=true;coorO[8][1+5]=true;coorO[8][2+5]=true;coorO[8][3+5]=true;
-
-  Boolean[][] coorS = new Boolean[9][ancho];coorS[0][0+10]=true;coorS[0][1+10]=true;coorS[0][2+10]=true;coorS[0][3+10]=true;coorS[1][0+10]=true;coorS[2][0+10]=true;coorS[3][0+10]=true;coorS[4][0+10]=true;coorS[4][1+10]=true;coorS[4][2+10]=true;coorS[4][3+10]=true;coorS[5][3+10]=true;coorS[6][3+10]=true;coorS[7][3+10]=true;coorS[8][0+10]=true;coorS[8][1+10]=true;coorS[8][2+10]=true;coorS[8][3+10]=true;
-
-  Boolean[][] coorE = new Boolean[9][ancho];coorE[0][0+15]=true;coorE[0][1+15]=true;coorE[0][2+15]=true;coorE[0][3+15]=true;coorE[1][0+15]=true;coorE[2][0+15]=true;coorE[3][0+15]=true;coorE[4][0+15]=true;coorE[4][1+15]=true;coorE[4][2+15]=true;coorE[4][3+15]=true;coorE[5][0+15]=true;coorE[6][0+15]=true;coorE[7][0+15]=true;coorE[8][0+15]=true;coorE[8][1+15]=true;coorE[8][2+15]=true;coorE[8][3+15]=true;
 
   /**
    * Imprime las letras dependiendo sus cordenadas en una matriz 2x2
    */
-  public static void imprimir(CaracterCoordenadas a, String s) {
-    char[] charArray = s.toCharArray();
-    int ancho = charArray.length * 5;
-    Character[][] lienzo = new Character[7][ancho];
-    Boolean[][] coor = a.getCoor();
-    Map<Character, CaracterCoordenadas> map = new HashMap<>();
-    map.put('J', a); 
+  
+  public static void imprimir(String s, Map<Character, CaracterCoordenadas> abecedario) {
+    
+    char[] charXSeparado = s.toCharArray();
+    int ancho = charXSeparado.length * 5;
+    Character[][] lienzo = new Character[9][ancho];
+    
+    
+    for (int m = 0; m < charXSeparado.length; m++) {
 
-    for (int m = 0; m < charArray.length; m++) {
-      if (charArray[m].equals){
+      Character letra = (Character) charXSeparado[m];
 
-      }
-    }
+      if (abecedario.containsKey(letra)) {
+        CaracterCoordenadas a = abecedario.get(letra);
+        Boolean[][] coor = a.getCoor();
+        
+        for (int i = 0; i < 9; i++) {
+          for (int j = 0; j < ancho; j++) {
+            if (coor[i][j] != null || coor[i][j] != null) {
+              lienzo[i][j ] = '*';
+            } else {
+              lienzo[i][j] = '-';
+            }
+          }
+        }
 
-    for (int i = 0; i < 7; i++) {
-      for (int j = 0; j < ancho; j++) {
-        if (coor[i][j] != null) {
-          lienzo[i][j] = '*';
-        } else {
-          lienzo[i][j] = ' ';
+        for (int k = 0; k < 9; k++) {
+          for (int l = 0; l < ancho; l++) {
+            System.out.print(lienzo[k][l]);
+          }
+          System.out.println();
         }
       }
     }
-
-    for (int k = 0; k < 7; k++) {
-      for (int l = 0; l < ancho; l++) {
-        System.out.print(lienzo[k][l]);
-      }
-      System.out.println();
-    }
-
     System.out.println("Gracias por usar mi programa. by - Mingood.");
+    
   }
 
 }
--- retoKevin/Impresora.java
+++ retoKevin/Impresora.java
@@ -1,12 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Impresora {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    int ancho = 5 * 4;
+    /* int ancho = 5 * 4; */
 
-    Boolean[][] coorj = new Boolean[9][ancho];
+    Boolean[][] coorj = new Boolean[9][20];
     coorj[0][3] = true;
     coorj[1][3] = true;
     coorj[2][3] = true;
@@ -22,103 +24,115 @@ public class Impresora {
     coorj[8][2] = true;
     coorj[8][3] = true;
 
-    // CaracterCoordenadas j = new CaracterCoordenadas('J', coorj);
+    CaracterCoordenadas J = new CaracterCoordenadas('J', coorj);
 
-    Boolean[][] coorO = new Boolean[9][ancho];
-    coorO[0][0 + 5] = true;
-    coorO[0][1 + 5] = true;
-    coorO[0][2 + 5] = true;
-    coorO[0][3 + 5] = true;
-    coorO[1][0 + 5] = true;
-    coorO[1][3 + 5] = true;
-    coorO[2][0 + 5] = true;
-    coorO[2][3 + 5] = true;
-    coorO[3][0 + 5] = true;
-    coorO[3][3 + 5] = true;
-    coorO[2][3 + 5] = true;
-    coorO[4][0 + 5] = true;
-    coorO[4][3 + 5] = true;
-    coorO[5][0 + 5] = true;
-    coorO[5][3 + 5] = true;
-    coorO[6][0 + 5] = true;
-    coorO[6][3 + 5] = true;
-    coorO[7][0 + 5] = true;
-    coorO[7][3 + 5] = true;
-    coorO[8][0 + 5] = true;
-    coorO[8][1 + 5] = true;
-    coorO[8][2 + 5] = true;
-    coorO[8][3 + 5] = true;
+    Boolean[][] coorO = new Boolean[9][20];
+    coorO[0][0] = true;
+    coorO[0][1] = true;
+    coorO[0][2] = true;
+    coorO[0][3] = true;
+    coorO[1][0] = true;
+    coorO[1][3] = true;
+    coorO[2][0] = true;
+    coorO[2][3] = true;
+    coorO[3][0] = true;
+    coorO[3][3] = true;
+    coorO[2][3] = true;
+    coorO[4][0] = true;
+    coorO[4][3] = true;
+    coorO[5][0] = true;
+    coorO[5][3] = true;
+    coorO[6][0] = true;
+    coorO[6][3] = true;
+    coorO[7][0] = true;
+    coorO[7][3] = true;
+    coorO[8][0] = true;
+    coorO[8][1] = true;
+    coorO[8][2] = true;
+    coorO[8][3] = true;
 
-    Boolean[][] coorS = new Boolean[9][ancho];
-    coorS[0][0 + 10] = true;
-    coorS[0][1 + 10] = true;
-    coorS[0][2 + 10] = true;
-    coorS[0][3 + 10] = true;
-    coorS[1][0 + 10] = true;
-    coorS[2][0 + 10] = true;
-    coorS[3][0 + 10] = true;
-    coorS[4][0 + 10] = true;
-    coorS[4][1 + 10] = true;
-    coorS[4][2 + 10] = true;
-    coorS[4][3 + 10] = true;
-    coorS[5][3 + 10] = true;
-    coorS[6][3 + 10] = true;
-    coorS[7][3 + 10] = true;
-    coorS[8][0 + 10] = true;
-    coorS[8][1 + 10] = true;
-    coorS[8][2 + 10] = true;
-    coorS[8][3 + 10] = true;
+    CaracterCoordenadas O = new CaracterCoordenadas('O', coorO);
 
-    Boolean[][] coorE = new Boolean[9][ancho];
-    coorE[0][0 + 15] = true;
-    coorE[0][1 + 15] = true;
-    coorE[0][2 + 15] = true;
-    coorE[0][3 + 15] = true;
-    coorE[1][0 + 15] = true;
-    coorE[2][0 + 15] = true;
-    coorE[3][0 + 15] = true;
-    coorE[4][0 + 15] = true;
-    coorE[4][1 + 15] = true;
-    coorE[4][2 + 15] = true;
-    coorE[4][3 + 15] = true;
-    coorE[5][0 + 15] = true;
-    coorE[6][0 + 15] = true;
-    coorE[7][0 + 15] = true;
-    coorE[8][0 + 15] = true;
-    coorE[8][1 + 15] = true;
-    coorE[8][2 + 15] = true;
-    coorE[8][3 + 15] = true;
+    Boolean[][] coorS = new Boolean[9][20];
+    coorS[0][0] = true;
+    coorS[0][1] = true;
+    coorS[0][2] = true;
+    coorS[0][3] = true;
+    coorS[1][0] = true;
+    coorS[2][0] = true;
+    coorS[3][0] = true;
+    coorS[4][0] = true;
+    coorS[4][1] = true;
+    coorS[4][2] = true;
+    coorS[4][3] = true;
+    coorS[5][3] = true;
+    coorS[6][3] = true;
+    coorS[7][3] = true;
+    coorS[8][0] = true;
+    coorS[8][1] = true;
+    coorS[8][2] = true;
+    coorS[8][3] = true;
 
-    // CaracterCoordenadas o = new CaracterCoordenadas('O', coorO);
+    CaracterCoordenadas S = new CaracterCoordenadas('S', coorS);
 
-    Character[][] lienzo = new Character[9][ancho];
+    Boolean[][] coorE = new Boolean[9][20];
+    coorE[0][1] = true;
+    coorE[0][2] = true;
+    coorE[0][0] = true;
+    coorE[0][3] = true;
+    coorE[1][0] = true;
+    coorE[2][0] = true;
+    coorE[3][0] = true;
+    coorE[4][0] = true;
+    coorE[4][1] = true;
+    coorE[4][2] = true;
+    coorE[4][3] = true;
+    coorE[5][0] = true;
+    coorE[6][0] = true;
+    coorE[7][0] = true;
+    coorE[8][0] = true;
+    coorE[8][1] = true;
+    coorE[8][2] = true;
+    coorE[8][3] = true;
 
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < ancho; j++) {
-        if (coorj[i][j] != null || coorO[i][j] != null || coorS[i][j] != null || coorE[i][j] != null) {
-          lienzo[i][j] = '*';
-        } else {
-          lienzo[i][j] = ' ';
-        }
-      }
-    }
+    CaracterCoordenadas E = new CaracterCoordenadas('O', coorE);
 
-    for (int k = 0; k < 9; k++) {
-      for (int l = 0; l < ancho; l++) {
-        System.out.print(lienzo[k][l]);
-      }
-      System.out.println();
-    }
+    Map<Character, CaracterCoordenadas> abecedario = new HashMap<>();
+    abecedario.put('J', J);
+    abecedario.put('O', O);
+    abecedario.put('S', S);
+    abecedario.put('E', E);
 
-    System.out.println("Gracias por usar mi programa. by - Mingood.");
-
-    // System.out.println("Bienvenido al dibujador de nombres.");
-    // System.out.print("Ingresa un tu nombre: ");
-    // String s = scanner.nextLine().toUpperCase();
-    // System.out.println();
-
-    // CaracterCoordenadas.imprimir(o, s);
-
+    
+    
+    // Character[][] lienzo = new Character[9][ancho];
+    
+    // for (int i = 0; i < 9; i++) {
+      //   for (int j = 0; j < ancho; j++) {
+        //     if (coorj[i][j] != null || coorO[i][j] != null || coorS[i][j] != null || coorE[i][j] != null) {
+          //       lienzo[i][j] = '*';
+          //     } else {
+            //       lienzo[i][j] = ' ';
+            //     }
+            //   }
+            // }
+            
+            // for (int k = 0; k < 9; k++) {
+              //   for (int l = 0; l < ancho; l++) {
+                //     System.out.print(lienzo[k][l]);
+                //   }
+                //   System.out.println();
+                // }
+                
+                // System.out.println("Gracias por usar mi programa. by - Mingood.");
+                
+                System.out.println("Bienvenido al dibujador de nombres.");
+                System.out.print("Ingresa un tu nombre: ");
+                String s = scanner.nextLine().toUpperCase();
+                System.out.println();
+                
+                CaracterCoordenadas.imprimir(s, abecedario);
+                
     scanner.close();
   }
 
