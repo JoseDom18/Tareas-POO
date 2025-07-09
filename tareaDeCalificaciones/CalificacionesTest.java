154df8dfe88bab7c8756f0bea91741a532d8a2a4
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-07-09 09:56:46 -0600

    codigo mejorado

--- tareaDeCalificaciones/CalificacionesTest.java
+++ tareaDeCalificaciones/CalificacionesTest.java
@@ -10,24 +10,25 @@ public class CalificacionesTest{
 
     ArrayList<Materia> listaMaterias = new ArrayList<>();
 
-    for (int i = 0; i < 5; i++) {
-      int cal = -1;
-      do {
-        System.out.printf("Ingresa la calificación de %s: ", materias[i]);
-        if (sc.hasNextInt()) {
-          cal = sc.nextInt();
-          sc.nextLine();
-          if (cal < 0) {
-            System.out.println("La calificacion tiene que ser positiva.");
-          }
-        } else {
-          System.out.println("Este caracter no es valido.");
-          sc.next();
-        }
-      } while (cal < 0);
-      Materia a = new Materia(materias[i], cal);
-      listaMaterias.add(a);
-    }
+   /*  for (int i = 0; i < 5; i++) {
+     int cal = -1;
+     do {
+       System.out.printf("Ingresa la calificación de %s: ", materias[i]);
+       if (sc.hasNextInt()) {
+         cal = sc.nextInt();
+         sc.nextLine();
+         if (cal < 0) {
+           System.out.println("La calificacion tiene que ser positiva.");
+         }
+       } else {
+         System.out.println("Este caracter no es valido.");
+         sc.next();
+       }
+     } while (cal < 0);
+     Materia a = new Materia(materias[i], cal);
+     listaMaterias.add(a);
+   } */
+   Materia.pedirCalificaciones(materias, listaMaterias, sc);
 
     System.out.printf("El promedio de las siguientes calificaciones fue %d.%n", Materia.sacarPromedio(listaMaterias));
     
--- tareaDeCalificaciones/Materia.java
+++ tareaDeCalificaciones/Materia.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Materia {
 
@@ -35,6 +36,27 @@ public class Materia {
     int promedio = sumaCal / 5;
 
     return promedio;
+  }
+
+  public static void pedirCalificaciones(String[] materias, ArrayList<Materia> listaMaterias, Scanner sc) {
+    for (int i = 0; i < 5; i++) {
+      int cal = -1;
+      do {
+        System.out.printf("Ingresa la calificación de %s: ", materias[i]);
+        if (sc.hasNextInt()) {
+          cal = sc.nextInt();
+          sc.nextLine();
+          if (cal < 0) {
+            System.out.println("La calificacion tiene que ser positiva.");
+          }
+        } else {
+          System.out.println("Este caracter no es valido.");
+          sc.next();
+        }
+      } while (cal < 0);
+      Materia a = new Materia(materias[i], cal);
+      listaMaterias.add(a);
+    }
   }
 
 }
